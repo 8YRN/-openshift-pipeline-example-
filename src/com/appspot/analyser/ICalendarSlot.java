1a1be03d0cbe4afd5b70c4a2c6de2d3e227b48f7
8YRN <109942719+8YRN@users.noreply.github.com>
2020-12-29 17:43:05 +0000

    Update Repository.php

--- src/com/appspot/analyser/ICalendarSlot.java
+++ src/com/appspot/analyser/ICalendarSlot.java
@@ -8,4 +8,11 @@ public interface ICalendarSlot extends Comparable<ICalendarSlot> {
 
 	public String getTitle();
 	public void setTitle(TextConstruct title);
-	public String getD
+	public String getDescription();
+	public void setDescription(TextConstruct descr);
+	public Calendar getStartDate();
+	public void setStartDate(Calendar start);
+	public Calendar getEndDate();
+	public void setEndDate(Calendar end);	
+	public double getDuration();
+}
---
+++ src/com/appspot/analyser/IEvent.java
@@ -0,0 +1,6 @@
+package com.appspot.analyser;
+
+import java.util.Map;
+
+import com.appspot.datastore.SphereName;
+import com.google.gdata.data.calendar.CalendarEventEntr
