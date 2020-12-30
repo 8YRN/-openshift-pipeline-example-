0ea0ee9450a36b3e5530a731d2ac4bb71d6c3e93
8YRN <109942719+8YRN@users.noreply.github.com>
2020-12-30 08:41:27 +0000

    Update model.py

--- src/com/appspot/analyser/IEvent.java
+++ src/com/appspot/analyser/IEvent.java
@@ -3,4 +3,14 @@ package com.appspot.analyser;
 import java.util.Map;
 
 import com.appspot.datastore.SphereName;
-import com.google.gdata.data.calendar.CalendarEventEntr
+import com.google.gdata.data.calendar.CalendarEventEntry;
+
+public interface IEvent extends ICalendarSlot {
+	public boolean isRecurring();
+	public boolean canReschedule();
+	public Map<SphereName, Double> getSpheres();
+	public Pair<Double, Double> getDurationInterval();
+	public void setDurationInterval(Pair<Double, Double> newInterval);
+	public void makePersistent(int alternative);
+	public CalendarEventEntry getCalendarEvent();
+}
---
+++ src/com/appspot/analyser/InsertSuggestion.java
@@ -0,0 +1,28 @@
+
+package com.appspot.analyser;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import com.appspot.datastore.SphereName;
+import com.appspot.iclifeplanning.authentication.CalendarUtils;
+import com.google.gdata.data.DateTime;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.calendar.CalendarEventEntry;
+import com.google.gdata.data.extensions.When;
+import com.google.gdata.util.ServiceException;
+
+public class InsertSuggestion extends Suggestion {
+
+	public InsertSuggestion(BaseCalendarSlot slot) {
+		super(slot);
+	}
+
+	public InsertSuggestion(IEvent e) {
+		super(e);
+	}
