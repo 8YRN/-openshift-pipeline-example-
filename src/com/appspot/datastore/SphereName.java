3429e3fbcf53ad3162b6c8814b2c9b5bb61359c5
8YRN <109942719+8YRN@users.noreply.github.com>
2021-01-03 18:41:36 +0000

    Edits.

---
+++ src/com/appspot/datastore/SphereName.java
@@ -0,0 +1,25 @@
+package com.appspot.datastore;
+
+public enum SphereName {
+	HEALTH {
+		@Override
+		public Double defaultValue() {
+			return 0.25;
+		}
+	}, 
+	WORK {
+		@Override
+		public Double defaultValue() {
+			return 0.25;
+		}
+	}, 
+	FAMILY {
+		@Override
+		public Double defaultValue() {
+			return 0.25;
+		}
+	}, 
+	RECREATION {
+		@Override
+		public Double defaultValue() {
+			r
