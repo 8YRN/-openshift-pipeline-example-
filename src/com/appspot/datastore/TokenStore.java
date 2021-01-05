ea679e0420e21f22b546d5432148ffe6470141e8
8YRN <109942719+8YRN@users.noreply.github.com>
2021-01-05 18:04:01 +0000

    remove puts

---
+++ src/com/appspot/datastore/TokenStore.java
@@ -0,0 +1,18 @@
+package com.appspot.datastore;
+
+import javax.jdo.JDOObjectNotFoundException;
+import javax.jdo.PersistenceManager;
+
+public class TokenStore {
+
+	public static String getToken(String id) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			Token token = pm.getObjectById(Token.class, id);
+
+			return token.getToken();
+		} 
+		catch (JDOObjectNotFoundException e) {
+			return null;
+		} 
+		fi
