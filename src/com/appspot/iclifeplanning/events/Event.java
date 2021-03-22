b2f3a0c72c28e18dcfde34e1179888df19c9050e
8YRN <109942719+8YRN@users.noreply.github.com>
2021-03-22 20:55:48 +0000

    Add travis.yml.

--- src/com/appspot/iclifeplanning/events/Event.java
+++ src/com/appspot/iclifeplanning/events/Event.java
@@ -12,4 +12,14 @@ import com.appspot.analyser.Pair;
 import com.appspot.datastore.SphereName;
 import com.google.gdata.data.calendar.CalendarEventEntry;
 
-// TODO (a
+// TODO (amadurska): Ensure keywords come from both title & description
+public class Event extends BaseCalendarSlot implements IEvent {
+	private Set<String> keywords = new HashSet<String>();
+	private Set<Event> childEvents;
+	private CalendarEventEntry calendarEventEntry;
+	private String id;
+	private boolean canReschedule;
+	private boolean isRecurring;
+	private Pair<Double, Double> durationInterval;
+
+	public Event(CalendarEventEntr
