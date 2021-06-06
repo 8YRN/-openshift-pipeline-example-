3168c0ff8fd0c3af8e886e179c60cc0b6651b8b9
8YRN <109942719+8YRN@users.noreply.github.com>
2021-06-06 11:37:20 +0000

    Complete readme

---
+++ src/org/json/JSONString.java
@@ -0,0 +1,5 @@
+package org.json;
+/**
+ * The <code>JSONString</code> interface allows a <code>toJSONString()</code> 
+ * method so that a class can change the behavior of 
+ * <code>JSONObject.toSt
