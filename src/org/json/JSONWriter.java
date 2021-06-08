ea4f92e8b8726ea8bbb2bb28e1de8ed89e4142a7
8YRN <109942719+8YRN@users.noreply.github.com>
2021-06-08 05:33:57 +0000

    Small code simplification

---
+++ src/org/json/JSONWriter.java
@@ -0,0 +1,10 @@
+package org.json;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/*
+Copyright (c) 2006 JSON.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software a
