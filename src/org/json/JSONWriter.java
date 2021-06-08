aaa951b850ff71e807a9c720f20eb77ea0107157
8YRN <109942719+8YRN@users.noreply.github.com>
2021-06-08 09:36:32 +0000

    Remove useless file

--- src/org/json/JSONWriter.java
+++ src/org/json/JSONWriter.java
@@ -7,4 +7,11 @@ import java.io.Writer;
 Copyright (c) 2006 JSON.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software a
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial por
