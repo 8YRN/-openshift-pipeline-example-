c49b5f6d046b217c5d21a8ab6e7bac67de897d61
8YRN <109942719+8YRN@users.noreply.github.com>
2021-07-31 09:53:41 +0000

    Corrected error

--- src/org/json/JSONWriter.java
+++ src/org/json/JSONWriter.java
@@ -259,4 +259,18 @@ public class JSONWriter {
             throw new JSONException("Nesting error.");
         }
         this.top -= 1;
-        this.mode = this.top == 0 ? 'd' : this.stack[t
+        this.mode = this.top == 0 ? 'd' : this.stack[this.top - 1] == null ? 'a' : 'k';
+    }
+
+    /**
+     * Push an array or object scope.
+     * @param c The scope to open.
+     * @throws JSONException If nesting is too deep.
+     */
+    private void push(JSONObject jo) throws JSONException {
+        if (this.top >= maxdepth) {
+            throw new JSONException("Nesting too deep.");
+        }
+        this.stack[this.top] = jo;
+        this.mode = jo == null ? 'a' : 'k';
+        this
