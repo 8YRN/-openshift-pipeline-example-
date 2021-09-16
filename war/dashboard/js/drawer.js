239c1eafeebf524b933d776e1fb56ae0e7c33254
8YRN <109942719+8YRN@users.noreply.github.com>
2021-09-16 14:10:50 +0000

    mrg

--- war/dashboard/js/drawer.js
+++ war/dashboard/js/drawer.js
@@ -8,4 +8,18 @@
 
 
 
-(funct
+(function ($, loaded) {
+$.extend({ drawer: {
+	init: function () {
+		this.self   = null;
+		this.closer = '<div id="drw_close"><a href="#" class="drw_close" title="close">Hide</a></div>';
+		this.loader = '<div id="drw_loader">Loading...</div>';
+		this.setTabs();
+		$('#drw').html('<div id="drw_content"></div>');
+	},
+	
+	
+	
+	setTabs: function () {
+		var tabs  = $('#drw_tabs li');
+		var w     = Math.floor(100 / ta
