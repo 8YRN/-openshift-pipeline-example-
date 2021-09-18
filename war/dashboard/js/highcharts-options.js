d49426c3d12155b83dd6ce0e4e0e19a09b16b148
8YRN <109942719+8YRN@users.noreply.github.com>
2021-09-18 22:34:08 +0000

    corrected typos

---
+++ war/dashboard/js/highcharts-options.js
@@ -0,0 +1,13 @@
+Highcharts.setOptions({
+   colors: ["#DDDF0D", "#7798BF", "#55BF3B", "#DF5353", "#aaeeee", "#ff0066", "#eeaaee",
+	  "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+   chart: {
+	  /*backgroundColor: {
+		 linearGradient: [0, 0, 0, 400],
+		 stops: [
+			[0, 'rgba(255, 96, 96, 0)'],
+			[1, 'rgba(255, 255, 16, 0)']
+		 ]
+		 //opacity: 0,
+	  },*/
+	  //bac
