43cfa47704bfe9e15464674516379a4160004f8e
8YRN <109942719+8YRN@users.noreply.github.com>
2022-08-11 21:26:35 +0000

    const fix

--- war/dashboard/js/plan-achievement.js
+++ war/dashboard/js/plan-achievement.js
@@ -19,4 +19,25 @@ var serverDataPlan = {
 			 // Store data from server for future (when another sphere is chosen, no get method is required)
 			serverDataPlan.allSpheres = data.spheres;
 			
-			//alert(serverDataPlan.allSph
+			//alert(serverDataPlan.allSpheres[0].series[0].name),
+					
+			// Prepare settings of a chart
+			planAchievementOptions = {
+				chart: {
+					renderTo: 'plan-achievement', 
+					defaultSeriesType: 'line'
+				},
+				title: {
+					text: 'Planned vs. achieved sphere realisation'
+				},
+				subtitle: {
+					text: ''
+				},
+				xAxis: {
+					type: 'datetime'
+				},
+				yAxis: {
+					title: null,
+					labels: {
+						formatter: function() {
+							return Highcharts.numberFormat(t
