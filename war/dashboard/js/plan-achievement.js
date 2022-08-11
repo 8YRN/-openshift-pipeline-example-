0bd05f91aa77631ae56daf35dd512a3fb84884d7
8YRN <109942719+8YRN@users.noreply.github.com>
2022-08-11 21:17:27 +0000

    rec

--- war/dashboard/js/plan-achievement.js
+++ war/dashboard/js/plan-achievement.js
@@ -8,4 +8,15 @@ var serverDataPlan = {
 		var planAchievementOptions;
 		
 								
-		//
+		// gets data from the server
+		$.getJSON("plan-achievement", parametersForServlet, function(data){
+		
+			if('nullData'==data.error)
+			{
+				alert('Weekly Profiles for user ' + parametersForServlet.userName + ' not found.');
+			}
+			
+			 // Store data from server for future (when another sphere is chosen, no get method is required)
+			serverDataPlan.allSpheres = data.spheres;
+			
+			//alert(serverDataPlan.allSph
