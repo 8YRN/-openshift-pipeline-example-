78c0bc7eafcdbdc87ac53eade75fa14be2e93a00
8YRN <109942719+8YRN@users.noreply.github.com>
2023-04-10 17:11:37 +0000

    Fix #172

--- war/js/jquery.localscroll-1.2.7-min.js
+++ war/js/jquery.localscroll-1.2.7-min.js
@@ -3,4 +3,7 @@
  * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
  * Dual licensed under MIT and GPL.
  * Date: 3/11/2009
- * @auth
+ * @author Ariel Flesler
+ * @version 1.2.7
+ **/
+;(function($){var l=location.href.replace(/#.*/,'');var g=$.localScroll=function(a){$('body').localScroll(a)};g.defaults={duration:1e3,axis:'y',event:'click',stop:true,target:window,reset:true};g.hash=function(a){if(location.hash){a=$.extend({},g.defaults,a);a.hash=false;if(a.reset){var e=a.duration;delete a.duration;$(a.target).scrollTo(0,a);a.duration=e}i(0,location,a)}};$.fn.loca
